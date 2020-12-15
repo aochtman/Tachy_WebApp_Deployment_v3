f138206486b54d89cf675d265ebcf0eacfc1fada
ZX02 <34794550+Zerui18@users.noreply.github.com>
2020-12-15 16:17:50 +0800

    Update app.py
    use cloud db

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def render_egm3(current_id: int, df_all):
 
 
 """  Connect to db   """
-db = DatabaseController('root', '4854197saw', 'tachy', 'localhost')  # local DB
-# db = DatabaseController.get_heroku_db()  #cloud dB
+# db = DatabaseController('root', '4854197saw', 'tachy', 'localhost')  # local DB
+db = DatabaseController.get_heroku_db()  #cloud dB
 caseids = db.fetch_all_case_ids()
 CASE_ID = '19_50_48'
 
